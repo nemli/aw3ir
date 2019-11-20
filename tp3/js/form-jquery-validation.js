6679a350d21d32d87011f598446ff5790655fd7c
nemli <56520783+nemli@users.noreply.github.com>
2019-11-20 23:33:10 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -17,7 +17,7 @@ $( document ).ready(function() {
 }
  else{
     $(".modal-title").html('<strong>Bienvenue</strong> '+ document.querySelector("#Prenom").value);
-    $(".modal-body").html('<strong>Vous êtes nés le</strong>'+dateValue+' <strong>et vous êtes à:</strong> <img src="https://maps.googleapis.com/maps/api/staticmap?markers= '+ adresseValue +'&zoom=12&size=240x200&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"></img> ' + adresseValue+'');
+    $(".modal-body").html('<strong>Vous êtes nés le</strong>'+dateValue+' <strong>et vous êtes à:</strong> <img width="480" src="https://maps.googleapis.com/maps/api/staticmap?markers= '+ adresseValue +'&zoom=12&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"></img> ' + adresseValue+'');
     
     $('#myModal').modal("show");
  }
