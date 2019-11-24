cfcb17db627d14d74da6d02eb1658918c97064db
nemli <56520783+nemli@users.noreply.github.com>
2019-11-24 01:18:53 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -3,20 +3,18 @@ $( document ).ready(function() {
     // voir plus : https://www.w3schools.com/js/js_htmldom.asp
    
      console.log( "DOM ready!" );
-     
-     // Y mettre le code jQuery pour valider tous les champs du formulaire
  });
     function validation(){
-    var nomValue= document.querySelector("#nom").value;
-    var prenomValue= document.querySelector("#Prenom").value;
-    var dateValue= document.querySelector("#datedenaissance").value;
-    var adresseValue= document.querySelector("#adresse").value;
-    var emailValue= document.querySelector("#email").value;
+    var nomValue= $("#nom").val();
+    var prenomValue= $("#Prenom").val();
+    var dateValue= $("#datedenaissance").val();
+    var adresseValue= $("#adresse").val();
+    var emailValue= $("#email").val();
     if(nomValue.length==0  || prenomValue.length==0 || dateValue.length==0 || adresseValue.length==0 || emailValue.length==0){
         $('#myModal').modal("show");
 }
  else{
-    $(".modal-title").html('<strong>Bienvenue</strong> '+ document.querySelector("#Prenom").value);
+    $(".modal-title").html('<strong>Bienvenue</strong> '+$("#Prenom").val());
     $(".modal-body").html('<strong>Vous êtes nés le</strong>'+dateValue+' <strong>et vous êtes à:</strong> <img width="480" src="https://maps.googleapis.com/maps/api/staticmap?markers= '+ adresseValue +'&zoom=12&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"></img> ' + adresseValue+'');
     
     $('#myModal').modal("show");
